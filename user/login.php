505d765607904e5984c5fff3447324c2162c576d
Your Name <pateldarsh3044@gmail.com>
2026-04-02 14:56:31 +0530

    update in login page

---
+++ user/login.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+include("../config/db_connection.php");
+
+if(isset($_POST['login'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) > 0){
+        $_SESSION['email'] = $email;
+        header("Location: dashboard.php");
+        exit();
+    } else {
+        $error = "Invalid Login!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Users Login</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+
+</style>
+</head>
+
+<body class="d-flex justify-content-center align-items-center">
+
+<div class="card-center p-4 shadow login-card" style="width: 350px;">
+    
+    <h3 class="text-center mb-4">Users Login</h3>
+
+    <?php if(isset($error)) { ?>
+        <div class="alert alert-danger"><?php echo $error; ?></div>
+    <?php } ?>
+
+    <form method="POST">
+        <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input type="email" name="email" class="form-control" required>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Password</label>
+            <input type="password" name="password" class="form-control" required>
+        </div>
+
+        <button name="login" class="btn btn-primary w-100">Login</button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
